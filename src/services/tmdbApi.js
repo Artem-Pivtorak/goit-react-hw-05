8507d18c6655b90e95a6fee6eae5a702cb8ff818
Pivtorak Artem <mulbrry.digital@gmail.com>
2025-04-24 13:59:20 +0300

    edit errors

--- src/services/tmdbApi.js
+++ src/services/tmdbApi.js
@@ -1,20 +1,20 @@
 import axios from 'axios';
 
 const BASE_URL = 'https://api.themoviedb.org/3';
-const API_TOKEN = '0bf2d873828383136252a9c89ed588e7';
+const API_KEY = '0bf2d873828383136252a9c89ed588e7';
 
 const options = {
   headers: {
-    Authorization: API_TOKEN,
-  },
+    Authorization: `Bearer ${API_KEY}`
+  }
 };
 
 export const fetchTrendingMovies = async () => {
-  const response = await axios.get(`${BASE_URL}/trending/movie/day?api_key=${API_TOKEN}`);
+  const response = await axios.get(`${BASE_URL}/trending/movie/day?api_key=${API_KEY}`);
   return response.data.results;
 };
 
-export const searchMovies = async (query) => {
+export const getMovieById = async (query) => {
   const response = await axios.get(
     `${BASE_URL}/search/movie?query=${query}&include_adult=false&language=en-US&page=1&api_key=${API_KEY}`
   );
